bf16080cc0facd1006d3cabc8a4d1d2bf49fc4c3
Hafidz Putra Herlyansyah <apiiettrecomen1@gmail.com>
2023-03-30 22:14:55 +0700

    update binding

--- app/src/main/java/com/challenge/challengechapter3/Adapter/AbjadAdapter.kt
+++ app/src/main/java/com/challenge/challengechapter3/Adapter/AbjadAdapter.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.challenge.challengechapter3.Data.DataWords
 import com.challenge.challengechapter3.DetailAbjadFragment
 import com.challenge.challengechapter3.R
+import com.challenge.challengechapter3.databinding.ButtonAbjadBinding
 
 
 class AbjadAdapter(val context: Context): RecyclerView.Adapter<AbjadAdapter.ViewHolder>() {
@@ -31,22 +32,20 @@ class AbjadAdapter(val context: Context): RecyclerView.Adapter<AbjadAdapter.View
 
     fun submitData(value: ArrayList<DataWords>) = differ.submitList(value)
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val button : Button = itemView.findViewById(R.id.btn)
-    }
+    inner class ViewHolder(val binding: ButtonAbjadBinding): RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.button_abjad, parent, false)
-        return ViewHolder(view)
+        val binding = ButtonAbjadBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = differ.currentList[position]
-        holder.button.text = data.listAbjad
-        holder.button.setOnClickListener{
+        holder.binding.btn.text = data.listAbjad
+        holder.binding.btn.setOnClickListener{
             val bundle = Bundle()
             bundle.putString("DATA_ABJAD", data.listAbjad)
             val detailAbjadFragment = DetailAbjadFragment()
